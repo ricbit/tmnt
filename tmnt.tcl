6d7c325981ce9fe0e8e368a61711db1f58aa7836
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-07-11 21:22:45 -0300

    Refactored tcl script

--- tmnt.tcl
+++ tmnt.tcl
@@ -1,5 +1,19 @@
 set throttle off
+
 debug set_bp 0x9f {} {type "tmnt"}
-debug set_watchpoint write_mem 0x104 {[debug read "memory" 0x103] == 0x9 && [debug read "memory" 0x104] == 0x2} {record start "/home/ricbit/work/tmnt/tmntmsx.avi"}
-debug set_watchpoint write_mem 0x104 {[debug read "memory" 0x103] == 0xe8 && [debug read "memory" 0x104] == 0x3} {record stop}
-debug set_watchpoint write_mem 0x104 {[debug read "memory" 0x103] == 0xe9 && [debug read "memory" 0x104] == 0x3} {quit}
+
+proc readmemw {addr} {
+  expr {
+    [debug read "memory" $addr] + 
+    256 * [debug read "memory" [expr {$addr + 1}]]
+  }
+}
+
+debug set_watchpoint write_mem 0x104 {[readmemw 0x103] == 521} {
+  record start "/home/ricbit/work/tmnt/tmntmsx.avi"
+}
+
+debug set_watchpoint write_mem 0x104 {[readmemw 0x103] == 1000} {
+  record stop
+  quit
+}
