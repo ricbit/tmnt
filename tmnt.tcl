d770aaa027227d6eb11aa8bfdc9420ab86623e87
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-07-12 14:35:26 -0300

    Tcl script can detect when vdp_command stops.

--- tmnt.tcl
+++ tmnt.tcl
@@ -1,6 +1,8 @@
 set throttle off
 set running 0
 set current_frame 0
+set vdp_command_running 0
+set fast_emulation 0
 
 proc readmemw {addr} {
   expr {
@@ -17,27 +19,38 @@ debug probe set_bp VDP.IRQvertical {
 }
 
 debug set_bp 0xC000 {$current_frame >= 750} {
-  puts stderr "smart_zblit starting at line [machine_info VDP_msx_y_pos]"
+  puts stderr "smart_zblit starting at [machine_info VDP_msx_y_pos]"
 }
 
 debug set_bp 0xC003 {} {
-  puts stderr "smart_zblit ending at line [machine_info VDP_msx_y_pos]"
+  puts stderr "smart_zblit ending at [machine_info VDP_msx_y_pos]"
 }
 
 debug set_bp 0xC006 {$current_frame >= 750} {
-  puts stderr "smart_palette starting at line [machine_info VDP_msx_y_pos]"
+  puts stderr "smart_palette starting at [machine_info VDP_msx_y_pos]"
 }
 
 debug set_bp 0xC009 {} {
-  puts stderr "smart_palette ending at line [machine_info VDP_msx_y_pos]"
+  puts stderr "smart_palette ending at [machine_info VDP_msx_y_pos]"
 }
 
 debug set_bp 0xC00C {$current_frame >= 750} {
-  puts stderr "smart_vdp_command starting at line [machine_info VDP_msx_y_pos]"
+  puts stderr "smart_vdp_command starting at [machine_info VDP_msx_y_pos]"
 }
 
 debug set_bp 0xC00F {} {
-  puts stderr "smart_vdp_command ending at line [machine_info VDP_msx_y_pos]"
+  set vdp_command_running 1
+  puts stderr "smart_vdp_command ending at [machine_info VDP_msx_y_pos]"
+}
+
+if {$fast_emulation == 0} {
+  debug set_condition {
+    $vdp_command_running == 1 && 
+    [expr {[debug read {VDP status regs} 2] & 1}] == 0
+  } {
+    set vdp_command_running 0
+    puts stderr "smart_vdp_command stopping at [machine_info VDP_msx_y_pos]"
+  }
 }
 
 debug set_watchpoint write_mem 0x104 {[readmemw 0x103] == 521} {
