37fc1c3187ee68b062c068d7566309278fd884f6
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-08-18 23:28:56 -0300

    Improve preloading of turtle posters

--- tools/gen_poster_raw.py
+++ tools/gen_poster_raw.py
@@ -6,7 +6,21 @@ raw = [ord(i) for i in open("raw/turtles.raw", "rb").read()]
 large = [0] * (512 * 212)
 for i in xrange(212):
   large[i * 512 : i * 512 + 128] = raw[i * 256 : i * 256 + 128]
-  large[i * 512 + 256: i * 512 + 256 + 128] = raw[i * 256 + 128: i * 256 + 256]
+start = 256 - 20
+size = 20
+hscroll = 100
+for i in xrange(26):
+  for j in xrange(4):
+    top = 103 - i * 4 - j
+    bottom = 108 + i * 4 + j
+    offset = hscroll + 256 - size
+    large[top * 512 + offset: top * 512 + size + offset] = raw[
+        top * 256 + 130 : top * 256 + 130 + size]
+    large[bottom * 512 + offset: bottom * 512 + size + offset] = raw[
+        bottom * 256 + 130 : bottom * 256 + 130 + size]
+  start -= 4
+  size += 4
+  hscroll -= 4
 left = [0] * (256 * 212)
 right = [0] * (256 * 212)
 for i in xrange(212):
