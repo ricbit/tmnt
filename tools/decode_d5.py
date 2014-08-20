58f5b90c0e980099f2dbc4458ac44339f62cff63
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-08-20 19:10:29 -0300

    Improved tool to decode vram diffs

--- tools/decode_d5.py
+++ tools/decode_d5.py
@@ -1,14 +1,22 @@
 import sys
+
 raw = [ord(i) for i in open(sys.argv[1], "rb").read()]
+out = [0] * (128 * 256)
+start = 0x18000
+page = 0
 pos = 0
 while raw[pos] != 0:
   if raw[pos] >= 128 + 64:
-    print "page: ", raw[pos] & 0x3F
+    page = raw[pos] & 0x3F
     pos += 1
   elif raw[pos] >= 128:
-    print "addr: ", ((raw[pos] & 0x3F) << 8) + raw[pos + 1]
+    addr = ((raw[pos] & 0x3F) << 8) + raw[pos + 1]
     pos += 2
   else:
     size = raw[pos]
-    print "raw ", size
+    dest = (page << 14) + addr - start
+    out[dest : dest + size] = raw[pos + 1 : pos + size + 1]
     pos += size + 1
+f = open(sys.argv[2], "wb")
+f.write("".join(chr(i) for i in out))
+f.close()
