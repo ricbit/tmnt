af63cb5cea5ea2a16a64666aeed367e4eca4bd51
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-08-19 22:25:14 -0300

    Refactoring convert_raw

--- tools/convert_raw.py
+++ tools/convert_raw.py
@@ -2,12 +2,17 @@
 
 import sys
 
-def save_sc5(raw, filename, start, size):
-  f = open(filename, "wb")
+def convert_sc5(raw, start, size):
+  out = []
   start *= 256
   for i in xrange(128 * size):
-    a = (ord(raw[start + i*2 + 0]) << 4) + ord(raw[start + i*2 + 1])
-    f.write(chr(a))
+    out.append(((raw[start + i*2 + 0]) << 4) + (raw[start + i*2 + 1]))
+  return out
+
+def save_sc5(raw, filename, start, size):
+  out = convert_sc5([ord(i) for i in raw], start, size)
+  f = open(filename, "wb")
+  f.write("".join(chr(i) for i in out))
   f.close()
 
 if __name__ == '__main__':
--- tools/gen_poster_raw.py
+++ tools/gen_poster_raw.py
@@ -26,6 +26,13 @@ right = [0] * (256 * 212)
 for i in xrange(212):
   left[i * 256 : i * 256 + 256] = large[i * 512 : i * 512 + 256]
   right[i * 256 : i * 256 + 256] = large[i * 512 + 256: i * 512 + 512]
+right_sc5 = c.convert_sc5(right, 0, 212)
+zero_sc5 = [0] * (128 * 212)
 c.save_sc5("".join(chr(i) for i in left), "poster_left.sc5", 0, 212)
-c.save_sc5("".join(chr(i) for i in right), "poster_right.sc5", 0, 212)
+
+def save_diff(newsc5, oldsc5, start, name):
+  page = start >> 14
+  addr = start and 0x3FFF
+
+save_diff(right_sc5, zero_sc5, 0x18000, "poster_right.d5")
 
