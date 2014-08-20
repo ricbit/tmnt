8417235af518ff439bfe66d683d559107c9c4ec3
Ricardo Bittencourt <bluepenguin@gmail.com>
2014-08-20 16:43:09 -0300

    Change git_graph.py to accept out-of-order commits

--- tools/git_graph.py
+++ tools/git_graph.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 git = subprocess.check_output("tools/git_graph.sh", shell=True)
 commits = re.findall("(?s)Date:\s+\w+\s+(\w+.*?:\d\d) .*?(\d+)\s+attract", git)
-x = [datetime.strptime(i[0] + " 2014", "%b %d %X %Y") 
-     for i in reversed(commits)]
-y = [i[1] for i in reversed(commits)]
+commits = [(datetime.strptime(i[0] + " 2014", "%b %d %X %Y"), i[1])
+           for i in commits]
+commits.sort()
 fig = plot.figure()
 fig.add_subplot(111)
-plot.plot(x, y)
+plot.plot([i[0] for i in commits], [i[1] for i in commits])
 fig.autofmt_xdate()
 plot.show()
