82a5d08910231376a6ed27cee2111b1044a09c9c
Ricardo Bittencourt <bluepenguin@gmail.com>
2015-06-04 19:01:32 -0300

    Timeline split into packages of 100 frames

--- tools/build_frames_page.py
+++ tools/build_frames_page.py
@@ -1,4 +1,24 @@
-print "<html><body>"
-for i in xrange(1, 980):
-  print 'Frame %03d<br><img src="msxframes/tmnt%03d.png"><br>' % (i + 517, i)
-print "</body></html>"
+def frame(i):
+  out = []
+  out.append('Frame %03d<br><div>' % i)
+  out.append('<img src="tmnt%03d.png">' % (i - 517))
+  out.append('<span style="width:20px; height:200px;background:red; float:left;">ahha')
+  out.append('</span><br style="clear: both;">\n')
+  return ''.join(out)
+
+f = open("frames.html", "wt")
+f.write("<html><body>")
+for i in xrange(500, 517+980, 50):
+  f.write('<a href="msxframes/frame%04d.html">Frames %d-%d</a><br>' % 
+          (i, i, i + 99))
+  g = open("msxframes/frame%04d.html" % i, "wt")
+  g.write("<html><body>")
+  for j in xrange(i, i + 100):
+    if j > 517 and j <= 517 + 980:
+      g.write(frame(j))
+  g.write("</body></html>")
+  g.close()    
+f.write("</body></html>")
+f.close()
+
+
