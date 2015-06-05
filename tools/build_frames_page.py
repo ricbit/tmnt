81ce7f2d9f0aa5f9bc8dcfe24901ecdd0b3f0e64
Ricardo Bittencourt <bluepenguin@gmail.com>
2015-06-05 02:55:31 -0300

    Timeline logs some cpu events

--- tools/build_frames_page.py
+++ tools/build_frames_page.py
@@ -2,28 +2,62 @@ import re, itertools
 
 screenheight = 30 + 41 + 192
 
+colormap = {
+  "palette": "red",
+  "zblit": "blue",
+  "vdp_command": "green"
+}
+
+def convert(line, curlines):
+  if curlines == 192:
+    if line >= 192:
+      return line - 192
+    else:
+      return line + 41 + 30
+  else:
+    if line >= 212:
+      return line - 212
+    else:
+      return line + 41 + 10
+
 f = open("log.txt", "rt")
 lines = []
 framelines = []
 smart = None
-current = 0
+current = -1
 for line in f:
   m = re.match("Frame (\d+), lines=(\d+)", line)
   if m is not None:
+    framenumber = m.group(1)
     curlines = int(m.group(2))
     lines.append(curlines)
-    framelines.append([0] * screenheight)
+    framelines.append(["#000"] * screenheight)
+    start = 0    
+    current += 1
+    continue
+  m = re.match("smart_(\w+) (?:starting|queued) at (\d+)", line)
+  if m is not None:
+    smart = m.group(1)
+    start = convert(int(m.group(2)), curlines)
+    continue
+  m = re.match("smart_(\w+) ending at (\d+)", line)
+  if m is not None:
+    end = convert(int(m.group(2)), curlines)
+    print framenumber,start,end
+    framelines[current][start:end+1] = [colormap[smart]] * (end - start + 1)
+    continue
+    
 f.close()
 
 def frame(i):
   out = []
   out.append('Frame %03d<br>' % (i + 518))
-  out.append('<div style="width:20px;height:%dpx;background:red;float:left;">'%
+  out.append('<div style="width:20px;height:%dpx;background:black;float:left;">'%
              screenheight)
   for k, v in itertools.groupby(framelines[i]):
     V = list(v)
-    out.append('<div style="width:10px; height:%dpx;background:#000"></div>' % 
-               len(V))
+    out.append('<div style="width:20px; height:%dpx;background:%s"></div>' % 
+               (len(V), k))
   out.append('</div>')
   spacing = (30+41-24) if lines[i] == 192 else (10+41-14)
   out.append('<div><div style="height:%dpx;"></div>' % spacing)
