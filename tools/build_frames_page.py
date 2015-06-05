c579be5abd3facbfc6a3d8a6729b1dc7b0a05c36
Ricardo Bittencourt <bluepenguin@gmail.com>
2015-06-05 10:38:01 -0300

    More cpu events on timeline

--- tools/build_frames_page.py
+++ tools/build_frames_page.py
@@ -3,9 +3,10 @@ import re, itertools
 screenheight = 30 + 41 + 192
 
 colormap = {
-  "palette": "red",
-  "zblit": "blue",
-  "vdp_command": "green"
+  "smart_palette": "red",
+  "smart_zblit": "blue",
+  "smart_vdp_command": "green",
+  "diffblit": "#5050ff"
 }
 
 def convert(line, curlines):
@@ -28,6 +29,10 @@ current = -1
 for line in f:
   m = re.match("Frame (\d+), lines=(\d+)", line)
   if m is not None:
+    if smart is not None:
+      end = convert(curlines - 1, curlines)
+      print framenumber,start,end
+      framelines[current][start:end+1] = [colormap[smart]] * (end - start + 1)
     framenumber = m.group(1)
     curlines = int(m.group(2))
     lines.append(curlines)
@@ -35,23 +40,26 @@ for line in f:
     start = 0    
     current += 1
     continue
-  m = re.match("smart_(\w+) (?:starting|queued) at (\d+)", line)
+  m = re.match("(\w+) (?:starting|queued) at (-?\d+)", line)
   if m is not None:
     smart = m.group(1)
     start = convert(int(m.group(2)), curlines)
     continue
-  m = re.match("smart_(\w+) ending at (\d+)", line)
+  m = re.match("(\w+) ending at (-?\d+)", line)
   if m is not None:
     end = convert(int(m.group(2)), curlines)
     print framenumber,start,end
     framelines[current][start:end+1] = [colormap[smart]] * (end - start + 1)
+    smart = None
     continue
     
 f.close()
 
+FIRST = 521
+
 def frame(i):
   out = []
-  out.append('Frame %03d<br>' % (i + 518))
+  out.append('Frame %03d<br>' % (i + FIRST))
   out.append('<div style="width:20px;height:%dpx;background:black;float:left;">'%
              screenheight)
   for k, v in itertools.groupby(framelines[i]):
@@ -67,14 +75,14 @@ def frame(i):
 
 f = open("frames.html", "wt")
 f.write("<html><body>")
-for i in xrange(500, 517+980, 50):
+for i in xrange(500, FIRST-1+980, 50):
   f.write('<a href="msxframes/frame%04d.html">Frames %d-%d</a><br>' % 
           (i, i, i + 99))
   g = open("msxframes/frame%04d.html" % i, "wt")
   g.write("<html><body>")
   for j in xrange(i, i + 100):
-    if j > 517 and j < 517 + 980:
-      g.write(frame(j - 518))
+    if j >= FIRST and j < FIRST - 1 + 980:
+      g.write(frame(j - FIRST))
   g.write("</body></html>")
   g.close()    
 f.write("</body></html>")
